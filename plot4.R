e6eb5836361a870cbaf8c68a2c5ac34de3db6c95
bernamr <64870334+bernamr@users.noreply.github.com>
2020-06-23 17:27:20 -0300

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -41,8 +41,7 @@ plot4 <-with(final_data, {
        ylab="Global Active Power (kilowatts)", xlab="")
   lines(Sub_metering_2~dateTime,col='Red')
   lines(Sub_metering_3~dateTime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Global_reactive_power~dateTime, type="l", 
        ylab="Global Rective Power (kilowatts)",xlab="")
 })
